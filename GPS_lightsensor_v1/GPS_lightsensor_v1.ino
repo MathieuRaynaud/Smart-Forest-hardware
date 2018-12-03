b7498d36d8c203376c0f7bf7ee0664e881c90662
Jblaffosse <37617043+Jblaffosse@users.noreply.github.com>
2018-12-03 11:23:55 +0100

    Light sensor is updated and GPS works well

--- GPS_lightsensor_v1/GPS_lightsensor_v1.ino
+++ GPS_lightsensor_v1/GPS_lightsensor_v1.ino
@@ -16,9 +16,11 @@ void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
 //   Connect the GPS RX (receive) pin to Digital 11
 SoftwareSerial mySerial(10, 11);
 Adafruit_GPS GPS(&mySerial);
+int transistorGPS = 12;      // allow you to switch on or off the GPS
+
 
 int lightSensor = A0;    // select the input pin for the potentiometer
-int transistorLightSensor = 13;      // select the pin for the LED
+int transistorLightSensor = 13;      // allow you to switch on or off the light sensor
 int lightSensorTimeResponse = 200; //wait 200 ms to have the steady value
 int userOrder = 0;    // User input order on the serial command
 
@@ -31,19 +33,10 @@ void setup() {
   pinMode(lightSensor, INPUT);
   pinMode(transistorLightSensor,OUTPUT);
   digitalWrite(transistorLightSensor,LOW);
+  pinMode(transistorGPS,OUTPUT);
+  digitalWrite(transistorGPS,LOW);
   
-  //Turn on the GPS serial and set the baud rate at 9600, 
-  GPS.begin(9600);
-  // to turn on RMC (recommended minimum) and GGA (fix data) including altitude
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // Set the update rate at 1 Hz 
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   
-  // Request updates on antenna status, comment out to keep quiet
-  GPS.sendCommand(PGCMD_ANTENNA);
-  // the nice thing about this code is you can have a timer0 interrupt go off
-  // every 1 millisecond, and read data from the GPS for you. that makes the
-  // loop code a heck of a lot easier!
-   useInterrupt(true);
+  
   
   if(DEBUG){
     Serial.println("Initialization Done!");
@@ -73,7 +66,29 @@ void loop() {
   }
   if(userOrder == 2)
   {
+    digitalWrite(transistorGPS,HIGH);
+    delay(2000);
+    if(DEBUG){
+      Serial.println("GPS is on");
+    }
     
+    //Turn on the GPS serial and set the baud rate at 9600, 
+  GPS.begin(9600);
+  // to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // Set the update rate at 1 Hz 
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+  // the nice thing about this code is you can have a timer0 interrupt go off
+  // every 1 millisecond, and read data from the GPS for you. that makes the
+  // loop code a heck of a lot easier!
+   useInterrupt(true);
+
+   delay(4000);
+   if(DEBUG){
+      Serial.println("Wait response");
+   }
     char c = GPS.read();
       // if a sentence is received, we can check the checksum, parse it...
     if (GPS.newNMEAreceived()) {
@@ -90,24 +105,31 @@ void loop() {
     Serial.print(GPS.minute, DEC); Serial.print(':');
     Serial.print(GPS.seconds, DEC); Serial.print('.');
     Serial.println(GPS.milliseconds);
-    if (GPS.fix) {
-      Serial.print("Location (in degrees, works with Google Maps): ");
-      Serial.print(GPS.latitudeDegrees, 4);
-      Serial.print(", "); 
-      Serial.println(GPS.longitudeDegrees, 4);
+    while(GPS.fix == 0) {
     }
+    
+    Serial.print("Location (in degrees, works with Google Maps): ");
+    Serial.print(GPS.latitudeDegrees, 4);
+    Serial.print(", "); 
+    Serial.println(GPS.longitudeDegrees, 4);
+      
     userOrder=0;
+    digitalWrite(transistorGPS,LOW);
+    if(DEBUG){
+      Serial.println("GPS is off");
+   }
   }
   
 }
 
 float lightValue(){
-  int sensorValue= 0;
+  float sensorValue= 0;
   for (int i=0;i<50;i++) {
     sensorValue += analogRead(lightSensor);
   }
-  sensorValue = sensorValue/50;
-  return (float)(1023-sensorValue)*10/sensorValue;;
+  sensorValue = sensorValue/50.0; //Take the mean of 50 values
+  sensorValue = sensorValue*(5.0/1023.0); //Convett into the voltage
+  return sensorValue/(10000.0*0.0000005); //Obtain the illuminance (lx) from the collector light current
 }
 
 void serialEvent()
